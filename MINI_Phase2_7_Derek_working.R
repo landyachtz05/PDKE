157891bd5e6bb7417542862f98d033db7d2062f8
landyachtz05 <djford@hawaii.edu>
2022-10-20 15:22:23 -1000

    Adding air temp graphs at the end of the script

--- MINI_Phase2_7_Derek_working.R
+++ MINI_Phase2_7_Derek_working.R
@@ -134,7 +134,6 @@ plot(HALE ,add = T, col="red")
 
 ##########   SHAPE FILES FOR ANALYSIS
 UNIT_X <-   c(HALE,HALE)
-
  
 ##########   ISLAND - NEED TO CHANGE MANUALLY BASED ON ISLAND
 UNIT_I <-  c("MN","MN")
@@ -228,6 +227,9 @@ RF_Map_Path <- ("E:/PDKE/CCVD/NEW RF MAPS/statewide_rf_mm/rf_mm/")
 
 #Replace this with the ONI Dataset at some point 
 MEI <- read.csv(paste0(IFOLDER,"MEI_Season.csv"),sep=",")
+
+########## Month Year Air Temperature Maps
+AT_Map_Path_A <- ("E:/PDKE/CCVD/CCVD INPUTS/air_temp/data_map/")
 
 #################################################################################################################################
 
@@ -4488,6 +4490,150 @@ dev.off()
 
 write.csv(Cell.MEI,paste0(RFOLDER,UNIT_N[u],"/",UNIT_N[u]," MEI_A.csv"),row.names = F)
 #}
+
+#####################################################
+##### Air temperature graph
+
+maps<-AT_Map_Path_A
+
+# make list of years
+years<-as.list(list.files(maps))
+
+# create empty table for raster stats
+table<-data.frame(matrix(ncol=5,nrow=0, 
+                         dimnames=list(NULL, c("year","month", "min", "max", "mean"))))
+
+# set row
+no<-1
+
+### for each year, loop through months and extract min, max, mean air temp value
+### from within the study area
+
+for (i in years) {
+
+  setwd(paste0(maps,i,"/"))
+  
+  # make list of monthly files
+  f<-as.list(list.files())
+  
+  # loop through monthly files and fill in table
+  for (x in f) {
+ 
+    # set year and month
+    y<-substr(x, nchar(x) - 10,nchar(x))
+    table[no,]$year<-substr(y,1,4)
+    m<-substr(x, nchar(x) - 5,nchar(x))
+    table[no,]$month<-substr(m,1,2)
+    
+    # load raster
+    map<-raster(x)
+    
+    # crop to study area
+    m2 <- crop(map, extent(HALE))
+    m3 <- mask(m2, HALE)
+    
+    # calculate min, max, mean air temp
+    table[no,]$min<-round(cellStats(m3, stat="min"), digits=2)
+    table[no,]$max<-round(cellStats(m3, stat="max"), digits=2)
+    table[no,]$mean<-round(cellStats(m3, stat="mean"), digits=2)
+    
+    no<-no+1
+  }
+}
+
+table
+
+# write to csv
+write.csv(table, paste0(RFOLDER,UNIT_N[u],"_monthly_airtemp.csv"))
+
+###### Monthly air temperature time series
+dat<-table
+
+head(dat)
+tail(dat)
+
+# convert all celcius to farenheit
+dat$min<-(dat$min*(9/5)) + 32
+dat$max<-(dat$max*(9/5)) + 32
+dat$mean<-(dat$mean * (9/5)) + 32
+
+# date column
+dat$date<-as.Date(paste0(dat$year,"-",dat$month,"-01"))
+
+##### add annual mean values (from code below this one)
+dat2<-merge(dat,dat.y[,c("year","mean")], by="year", all.x=T)
+head(dat2, 20)
+
+# set y-axis limits
+ylow<-min(dat2$mean.x)*.95
+yhi<-max(dat2$mean.x)*1.0005
+
+# set slope
+slope<-formatC((coef(lm(dat2$mean.x~dat2$date))[2]), format="e", digits=2)
+slope
+
+dpi=300
+
+png(paste0(RFOLDER,UNIT_N[u],"/",UNIT_N[u],"_monthly_airtemp.png"),width=6*dpi,height=3*dpi,res=dpi)
+
+ggplot(dat2, aes(x=date,y=mean.x)) +
+  geom_line(color="grey60") +
+  geom_smooth(span=0.2, se=F, size=1.3, color="orange") +
+  geom_smooth(method=lm, se=F, color="black") +
+  stat_cor(method="pearson", label.x=as.Date("2012-01-01"), label.y=58) +
+  scale_x_date(date_breaks = "4 years", labels = date_format(format="%Y")) +
+  ylim(ylow,yhi) +
+  labs(title="Monthly Air Temperature",
+       y="Temperature (F)", x="") +
+  geom_hline(yintercept=0) +
+  annotate("text", x=as.Date("2015-07-01"), y=56.8, 
+           label=paste0("Slope = ",slope)) +
+  theme(panel.background=element_rect(fill=NA, color="black"),
+        panel.grid.major=element_line(color="grey90"),
+        panel.grid.minor=element_blank())
+
+dev.off()
+
+#############################
+### Annual air temp trend
+
+# aggregate monthly to annual air temp
+dat.y<-aggregate(mean ~ year, dat, mean)
+dat.y$mean<-round(dat.y$mean, digit=2)
+dat.y$meanmin<-round((aggregate(mean ~ year,dat, min))$mean, digits=2)
+dat.y$meanmax<-round((aggregate(mean ~ year,dat, max))$mean, digits=2)
+dat.y$min<-round((aggregate(min ~ year, dat, min))$min, digits=2)
+dat.y$max<-round((aggregate(max ~ year, dat, max))$max, digits=2)
+
+head(dat.y)
+
+# make date column
+dat.y$date<-as.Date(paste0(dat.y$year,"-01-01"))
+
+slope<-formatC((coef(lm(dat.y$mean~dat.y$date))[2]), format="e", digits=2)
+slope
+
+png(paste0(RFOLDER,UNIT_N[u],"/",UNIT_N[u],"_annual_airtemp.png"),width=6*dpi,height=3*dpi,res=dpi)
+
+ggplot(dat.y, aes(x=date, y=mean)) +
+  geom_line(size=1.2, color="orange") +
+  geom_smooth(method=lm, se=F, size=1, color="black") +
+  stat_cor(method="pearson", label.x=as.Date("1991-01-01"), label.y=58) +
+  scale_x_date(date_breaks = "2 years", labels = date_format(format="%Y")) +
+  labs(title="Annual Air Temperature and Extremes",
+       y = "Temperature (F)", x = "Year") +
+  geom_line(aes(x=date,y=min), size=1.2, color="blue") +
+  geom_line(aes(x=date,y=max), size=1.2, color="red") +
+  annotate("text", x=as.Date("1994-07-01"), y=54, 
+           label=paste0("Slope = ",slope)) +
+  theme(panel.background=element_rect(fill=NA, color="black"),
+        panel.grid.major=element_line(color="grey90"),
+        panel.grid.minor=element_blank())
+
+dev.off()
+
+
+
 
 #END OF LOOP 
 write.csv(Cell.DataCL,paste0(SFOLDER,UNIT_N[u],"_CL.csv"),row.names = F)
