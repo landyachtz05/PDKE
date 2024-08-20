af3cba859e4a0fc6830117fdacd6eef78f126de8
jgeis <jgeis26@gmail.com>
2024-08-20 07:14:59 -1000

    Added  logos and got rid of the word "Polygon" in the UI.

--- PDKESite/ui.R
+++ PDKESite/ui.R
@@ -28,15 +28,18 @@ ui <- fluidPage(
     "))
   ),
   
+  # Display an image above the main content
+  img(src = "logo-PDKE_Color_Horizontal_Type-05.png", height = "200px", alt = "PDKE Logo", align="center"),
+  
   # Application title
-  titlePanel("Leaflet Map with Shapefile Selector"),
+  #titlePanel("Leaflet Map with Shapefile Selector"),
   
   # Sidebar layout with input and output definitions
   sidebarLayout(
     # Sidebar panel for inputs
     sidebarPanel(
       # Text input for email address
-      textInput("email", "Enter your email address:", value = "", placeholder = "your.email@example.com"),
+      textInput("email", "Your email address:", value = "", placeholder = "your.email@example.com"),
       # Warning message for email validation
       tags$div(id = "email_warning", style = "color: red; margin-top: 12px;",
         tags$p("An email address is required since results will be emailed to you once generated.  The submit button will not be enabled until a shape is selected or drawn and all values are provided.")
@@ -44,18 +47,16 @@ ui <- fluidPage(
       
       # Dropdown menu to select a shapefile
       uiOutput("shapefile_select"),
-      #textInput("polygon_name", "Polygon name:", ""),
-      #textInput("polygon_short_name", "Polygon short name:", ""),
       
       div(
         style = "display: flex; align-items: center;",
-        textInput("polygon_name", "Polygon name:", ""),
+        textInput("polygon_name", "Location name:", ""),
         div(id = "info_polygon_name", class = "info-circle", "?")
       ),
       # Add text input for polygon short name with an info button
       div(
         style = "display: flex; align-items: center;",
-        textInput("polygon_short_name", "Polygon short name:", ""),
+        textInput("polygon_short_name", "Location short name:", ""),
         div(id = "info_polygon_short_name", class = "info-circle", "?")
       ),
       
@@ -88,7 +89,30 @@ ui <- fluidPage(
       leafletOutput("map", height = 600),
       
       # Add a button to save the selected area as a shapefile
-      actionButton("save_button", "Save Selected Area as Shapefile")
+      actionButton("save_button", "Use Selected Area")
     )
+  ),
+  
+  # Header for the first row of images
+  h3(""),
+  h3("Partners:"),
+  # Two rows of three images each below the main content
+  #For Partners: HCDP, USFS, UHM and Clark University
+  fluidRow(
+    column(1),
+    column(2, img(src = "logo-USFS.png", height = "150x", alt = "USFS Logo")),
+    column(2, img(src = "logo-UH_Manoa.png", height = "150px", alt = "UHM Logo")),
+    column(2, img(src = "logo-clark-university.png", height = "150px", alt = "Clark University Logo")),
+    column(4, img(src = "logo-HCDP_Text_Bottom_Color.jpg", height = "150px", alt = "HCDP Logo"))
+  ),
+  h3("Funders:"),
+  #For Funders: NRCS, NIDIS, PI-CASC, EWC, CWRM
+  fluidRow(
+    column(1),
+    column(2, img(src = "logo-NRCS.png", height = "150px", alt = "NRCS Logo")),
+    column(2, img(src = "logo-nidis.png", height = "150px", alt = "NIDIS Logo")),
+    column(2, img(src = "logo-PI-CASC-marble-w-full-name.jpg", height = "150px", alt = "PI-CASC Logo")),
+    column(2, img(src = "logo-ewcsig-sqr_10inprnt.jpg", height = "150px", alt = "EWC Logo")),
+    column(2, img(src = "logo-cwrm.png", height = "150px", alt = "CWRM Logo"))
   )
 )
